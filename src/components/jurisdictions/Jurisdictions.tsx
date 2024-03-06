c7d27bd372ed50c564edea21a32494e338880831
FrancoPrandi <francoprandi55@gmail.com>
2024-03-06 11:44:25 -0300

    feat: responsive update

--- src/components/jurisdictions/Jurisdictions.tsx
+++ src/components/jurisdictions/Jurisdictions.tsx
@@ -7,9 +7,10 @@ import cordoba from "../../assets/cordoba.jpg";
 import laPampa from "../../assets/la-pampa.jpg";
 import { useRouter } from "next/navigation";
 import { motion, useAnimation, useInView } from "framer-motion";
+import "@/styles/jurisdiction.css";
 
 const Jurisdictions = () => {
-    const router = useRouter();
+  const router = useRouter();
   const animationControls1 = useAnimation();
   const ref1 = useRef(null);
   const inView1 = useInView(ref1);
@@ -59,11 +60,31 @@ const Jurisdictions = () => {
     router.push(`/${jurisdiction}`);
   };
   return (
-    <div className="flex flex-col w-full justify-evenly align-middle text-center items-center h-screen bg-white z-10">
+    <div className="flex flex-col w-full justify-evenly align-middle text-center items-center h-screen bg-white z-10 containerWithScroll">
+      <motion.div
+        initial="hidden"
+        viewport={{ once: true }}
+        animate={animationControls1}
+        variants={{
+          visible: {
+            opacity: 1,
+            y: 0,
+            transition: { duration: 1.5, delay: 0.3 },
+          },
+          hidden: { opacity: 0, y: 100 },
+        }}
+        ref={ref1 as any}
+        className="flex flex-row h-1/8 items-center font-montserrat text-gray-500 font-bold w-full px-60"
+      >
+        <hr className="w-2/6 border-gray-300 titleLine" />
+        <h2 className="w-2/6 text-xl titleText">JURISDICCIONES</h2>
+        <hr className="w-2/6 border-gray-300 titleLine" />
+      </motion.div>
+      <div className="flex flex-col w-full h-7/8 justify-center items-center gap-4">
         <motion.div
-          initial="hidden"
           viewport={{ once: true }}
-          animate={animationControls1}
+          initial="hidden"
+          animate={animationControls2}
           variants={{
             visible: {
               opacity: 1,
@@ -72,132 +93,112 @@ const Jurisdictions = () => {
             },
             hidden: { opacity: 0, y: 100 },
           }}
-          ref={ref1 as any}
-          className="flex flex-row h-1/8 items-center font-montserrat text-gray-500 font-bold w-full px-60"
+          ref={ref2 as any}
+          className="flex flex-row w-full gap-4 px-60 jurisdictionsContainer"
         >
-          <hr className="w-2/6 border-gray-300" />
-          <h2 className="w-2/6 text-xl">JURISDICCIONES</h2>
-          <hr className="w-2/6 border-gray-300" />
-        </motion.div>
-        <div className="flex flex-col w-full h-7/8 justify-center items-center gap-4">
-          <motion.div
-            viewport={{ once: true }}
-            initial="hidden"
-            animate={animationControls2}
-            variants={{
-              visible: {
-                opacity: 1,
-                y: 0,
-                transition: { duration: 1.5, delay: 0.3 },
-              },
-              hidden: { opacity: 0, y: 100 },
-            }}
-            ref={ref2 as any}
-            className="flex flex-row w-full gap-4 px-60"
-          >
-            <div className="border border-gray-300 w-1/3 h-80 rounded-md relative overflow-hidden shadow-lg card">
-              <motion.img
-                // layoutId="la-pampa"
-                src={laPampa.src}
-                alt="bsas"
-                className="object-cover rounded-md h-full w-full"
-              />
-              <div className="flex flex-col justify-center items-center absolute bottom-0 left-0 w-full bg-transp p-4 gap-2">
-                <h3>La Pampa</h3>
-                <button
-                  onClick={() => handleProcedure("la-pampa")}
-                  className="bg-red-700 hover:bg-red-800 text-white font-bold py-1 px-4 rounded"
-                >
-                  Consultar
-                </button>
-              </div>
-            </div>
-            <div className="border border-gray-300 w-1/3 h-80 rounded-md relative overflow-hidden shadow-lg card">
-              <motion.img
-                layoutId="cordoba"
-                src={cordoba.src}
-                alt="bsas"
-                className="object-cover rounded-md h-full w-full"
-              />
-              <div className="flex flex-col justify-center items-center absolute bottom-0 left-0 w-full bg-transp p-4 gap-2">
-                <h3>Cordoba</h3>
-                <button
-                  onClick={() => handleProcedure("cordoba")}
-                  className="bg-red-700 hover:bg-red-800 text-white font-bold py-1 px-4 rounded"
-                >
-                  Consultar
-                </button>
-              </div>
-            </div>
-            <div className="border border-gray-300 w-1/3 h-80 rounded-md relative overflow-hidden shadow-lg card">
-              <motion.img
-                layoutId="san-luis"
-                src={sanLuis.src}
-                className="object-cover rounded-md h-full w-full"
-              />
-              <div className="flex flex-col justify-center items-center absolute bottom-0 left-0 w-full bg-transp p-4 gap-2">
-                <h3>San Luis</h3>
-                <button
-                  onClick={() => handleProcedure("san-luis")}
-                  className="bg-red-700 hover:bg-red-800 text-white font-bold py-1 px-4 rounded"
-                >
-                  Consultar
-                </button>
-              </div>
-            </div>
-          </motion.div>
-          <motion.div
-            layoutId="jurisdic"
-            viewport={{ once: true }}
-            initial="hidden"
-            animate={animationControls3}
-            variants={{
-              visible: {
-                opacity: 1,
-                y: 0,
-                transition: { duration: 1.5, delay: 0.3 },
-              },
-              hidden: { opacity: 0, y: 100 },
-            }}
-            ref={ref3 as any}
-            className="flex flex-row w-full justify-center gap-4 px-60"
-          >
-            <div className="border border-gray-300 w-1/3 h-80 rounded-md relative overflow-hidden shadow-lg card">
-              <motion.img
-                layoutId="neuquen"
-                src={neuquen.src}
-                className="object-cover rounded-md h-full w-full"
-              />
-              <div
-                onClick={() => handleProcedure("neuquen")}
-                className="flex flex-col justify-center items-center absolute bottom-0 left-0 w-full bg-transp p-4 gap-2"
+          <div className="border border-gray-300 w-1/3 h-80 rounded-md relative overflow-hidden shadow-lg card jurisdictionCard">
+            <motion.img
+              // layoutId="la-pampa"
+              src={laPampa.src}
+              alt="bsas"
+              className="object-cover rounded-md h-full w-full"
+            />
+            <div className="flex flex-col justify-center items-center absolute bottom-0 left-0 w-full bg-transp p-4 gap-2 buttonContainer">
+              <h3>La Pampa</h3>
+              <button
+                onClick={() => handleProcedure("la-pampa")}
+                className="bg-red-700 hover:bg-red-800 text-white font-bold py-1 px-4 rounded consultButton"
               >
-                <h3>Neuquen</h3>
-                <button className="bg-red-700 hover:bg-red-800 text-white font-bold py-1 px-4 rounded">
-                  Consultar
-                </button>
-              </div>
+                Consultar
+              </button>
             </div>
-            <div className="border border-gray-300 w-1/3 h-80 rounded-md relative overflow-hidden shadow-lg card">
-              <motion.img
-                layoutId="buenos-aires"
-                src={buenosAires.src}
-                className="object-cover rounded-md h-full w-full"
-              />
-              <div className="flex flex-col justify-center items-center absolute bottom-0 left-0 w-full bg-transp p-4 gap-2">
-                <h3>Buenos Aires</h3>
-                <button
-                  onClick={() => handleProcedure("buenos-aires")}
-                  className="bg-red-700 hover:bg-red-800 text-white font-bold py-1 px-4 rounded"
-                >
-                  Consultar
-                </button>
-              </div>
+          </div>
+          <div className="border border-gray-300 w-1/3 h-80 rounded-md relative overflow-hidden shadow-lg card jurisdictionCard">
+            <motion.img
+              layoutId="cordoba"
+              src={cordoba.src}
+              alt="bsas"
+              className="object-cover rounded-md h-full w-full"
+            />
+            <div className="flex flex-col justify-center items-center absolute bottom-0 left-0 w-full bg-transp p-4 gap-2 buttonContainer">
+              <h3>Cordoba</h3>
+              <button
+                onClick={() => handleProcedure("cordoba")}
+                className="bg-red-700 hover:bg-red-800 text-white font-bold py-1 px-4 rounded consultButton"
+              >
+                Consultar
+              </button>
             </div>
-          </motion.div>
-        </div>
+          </div>
+          <div className="border border-gray-300 w-1/3 h-80 rounded-md relative overflow-hidden shadow-lg card jurisdictionCard">
+            <motion.img
+              layoutId="san-luis"
+              src={sanLuis.src}
+              className="object-cover rounded-md h-full w-full"
+            />
+            <div className="flex flex-col justify-center items-center absolute bottom-0 left-0 w-full bg-transp p-4 gap-2 buttonContainer">
+              <h3>San Luis</h3>
+              <button
+                onClick={() => handleProcedure("san-luis")}
+                className="bg-red-700 hover:bg-red-800 text-white font-bold py-1 px-4 rounded consultButton"
+              >
+                Consultar
+              </button>
+            </div>
+          </div>
+        </motion.div>
+        <motion.div
+          layoutId="jurisdic"
+          viewport={{ once: true }}
+          initial="hidden"
+          animate={animationControls3}
+          variants={{
+            visible: {
+              opacity: 1,
+              y: 0,
+              transition: { duration: 1.5, delay: 0.3 },
+            },
+            hidden: { opacity: 0, y: 100 },
+          }}
+          ref={ref3 as any}
+          className="flex flex-row w-full justify-center gap-4 px-60 jurisdictionsContainer"
+        >
+          <div className="border border-gray-300 w-1/3 h-80 rounded-md relative overflow-hidden shadow-lg card jurisdictionCard">
+            <motion.img
+              layoutId="neuquen"
+              src={neuquen.src}
+              className="object-cover rounded-md h-full w-full"
+            />
+            <div
+              onClick={() => handleProcedure("neuquen")}
+              className="flex flex-col justify-center items-center absolute bottom-0 left-0 w-full bg-transp p-4 gap-2 buttonContainer"
+            >
+              <h3>Neuquen</h3>
+              <button className="bg-red-700 hover:bg-red-800 text-white font-bold py-1 px-4 rounded consultButton">
+                Consultar
+              </button>
+            </div>
+          </div>
+          <div className="border border-gray-300 w-1/3 h-80 rounded-md relative overflow-hidden shadow-lg card jurisdictionCard">
+            <motion.img
+              layoutId="buenos-aires"
+              src={buenosAires.src}
+              className="object-cover rounded-md h-full w-full"
+            />
+            <div className="flex flex-col justify-center items-center absolute bottom-0 left-0 w-full bg-transp p-4 gap-2 buttonContainer">
+              <h3>Buenos Aires</h3>
+              <button
+                onClick={() => handleProcedure("buenos-aires")}
+                className="bg-red-700 hover:bg-red-800 text-white font-bold py-1 px-4 rounded consultButton"
+              >
+                Consultar
+              </button>
+            </div>
+          </div>
+        </motion.div>
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Jurisdictions
+export default Jurisdictions;
